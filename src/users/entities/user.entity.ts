d52c1b6c4f87a4ffc652e1d97807cea8ab58374c
Fladit <zatsarin77@gmail.com>
2021-04-19 21:43:37 +0300

    add a partial validation for user entity

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -2,6 +2,7 @@ import { Column, Entity, JoinColumn, ManyToOne, OneToMany, OneToOne, PrimaryGene
 import { Role } from "./role.entity";
 import { MaritalStatus } from "./marital-status.entity";
 import { Photo } from "./photo.entity";
+import { IsBoolean, IsDate, IsEmail, IsString, MaxLength, MinLength } from "class-validator";
 
 @Entity()
 export class User {
@@ -15,18 +16,32 @@ export class User {
   marital_status_code: MaritalStatus;
 
   @Column({unique: true, nullable: false})
+  @IsString()
+  @MinLength(3)
+  @MaxLength(16)
   username: string;
 
   @Column({nullable: false})
+  @IsString()
+  @MinLength(6)
+  @MaxLength(32)
   password: string;
 
   @Column({nullable: false})
+  @IsEmail()
+  @MaxLength(320)
   email: string;
 
   @Column({nullable: false})
+  @IsString()
+  @MinLength(2)
+  @MaxLength(16)
   firstName: string;
 
   @Column({nullable: false})
+  @IsString()
+  @MinLength(2)
+  @MaxLength(32)
   lastName: string;
 
   @OneToOne(() => Photo)
@@ -34,21 +49,29 @@ export class User {
   avatar: Photo;
 
   @Column()
+  @IsString()
+  @MaxLength(512)
   statusTitle: string;
 
   @Column({type: 'timestamp', nullable: false})
+  @IsDate()
   birthday: Date;
 
   @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP'})
   registrationDate: Date;
 
   @Column({default: false})
+  @IsBoolean()
   isConfirmed: boolean;
 
   @Column({default: false})
+  @IsBoolean()
   isBanned: boolean;
 
-  @Column({default: null})
+  @Column()
+  @IsString()
+  @MinLength(8)
+  @MaxLength(128)
   banReason: string;
 
   @OneToMany(() => Photo, photo => photo.user)
